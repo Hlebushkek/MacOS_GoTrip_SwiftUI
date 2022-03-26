14a92a112351dc4302d6986775be63756b1a832d
FallWaterFall <volsoor@gmail.com>
2022-03-26 14:14:21 +0200

    Initial Commit

---
+++ MacOS_GoTrip_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacOS_GoTrip_SwiftUI
+//
+//  Created by Gleb Sobolevsky on 26.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacOS_GoTrip_SwiftUI/MacOS_GoTrip_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_GoTrip_SwiftUIApp.swift
+//  MacOS_GoTrip_SwiftUI
+//
+//  Created by Gleb Sobolevsky on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_GoTrip_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
